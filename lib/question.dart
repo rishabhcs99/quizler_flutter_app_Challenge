dbfc42650c53ef1df138e3a932627e316482e5bf
Rishabh Shetty <rishabhcs99@gmail.com>
2020-06-11 15:18:35 +0530

    dartConst

--- lib/question.dart
+++ lib/question.dart
@@ -3,10 +3,7 @@ class Question
   String questionText;
   bool questionAnswer;
 
-  Question(String q, bool a)
-  {
-    questionText=q;
-    questionAnswer=a;
-  }
+  Question(this.questionText, this.questionAnswer);
+
 }
 Question newQuestion = Question('text',true);
